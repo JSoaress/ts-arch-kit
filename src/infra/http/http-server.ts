f1f6b643ea3a285ca934d7c2d3031e5f95c3fbf7
João Vitor <joao_vitorsgs@hotmail.com>
2024-04-09 19:06:46 -0300

    feat(http): :zap: altera metodo 'listen' de IHttpServer p/ retornar um http.Server

--- src/infra/http/http-server.ts
+++ src/infra/http/http-server.ts
@@ -1,6 +1,8 @@
+import * as http from "http";
+
 import { HTTPRequest, HTTPResponse, HttpMethods } from "./http";
 
 export interface IHttpServer {
     register(method: HttpMethods, url: string, callback: <T = unknown>(req: HTTPRequest) => Promise<HTTPResponse<T>>): void;
-    listen(port: number): void;
+    listen(port: number): http.Server;
 }
