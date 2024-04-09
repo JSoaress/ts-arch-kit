f5f59e6b91f51b7966dab53fbba1284a2d0cdbcb
João Vitor <joao_vitorsgs@hotmail.com>
2024-04-09 19:07:54 -0300

    feat(http): :zap: ajusta tipagem de retorno dos metodos da classe HttpStatusCodes

--- src/infra/http/HttpStatusCodes.ts
+++ src/infra/http/HttpStatusCodes.ts
@@ -2,16 +2,21 @@ import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
 export type HttpReasons = keyof typeof StatusCodes;
 
+type HttpStatus = {
+    code: number;
+    phrase: string;
+};
+
 export class HttpStatusCodes {
-    static getStatusCode(reason: HttpReasons) {
+    static getStatusCode(reason: HttpReasons): number {
         return StatusCodes[reason];
     }
 
-    static getReasonPhrase(reason: HttpReasons) {
+    static getReasonPhrase(reason: HttpReasons): string {
         return ReasonPhrases[reason];
     }
 
-    static getStatus(reason: HttpReasons) {
+    static getStatus(reason: HttpReasons): HttpStatus {
         return {
             code: StatusCodes[reason],
             phrase: ReasonPhrases[reason],
