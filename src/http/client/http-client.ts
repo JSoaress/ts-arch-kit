748eb24db5663f0726ee97d27464cde60447f50b
João Vitor <joao_vitorsgs@hotmail.com>
2025-03-23 16:34:07 -0300

    feat(http): add classe AbstractHttpClient que implementa IHttpClient
    a classe também segrega os metodos http em funcoes separadas, consumindo o metodo abstrato 'request'
    com o devido parametro 'method'

--- src/http/client/http-client.ts
+++ src/http/client/http-client.ts
@@ -19,13 +19,41 @@ type ErrorResponse<E> = {
     error: E;
 };
 
-export type HTTPClientResponse<T, E = Error> = (SuccessResponse<T> | ErrorResponse<E>) & {
+export type HTTPClientResponse<T, E> = (SuccessResponse<T> | ErrorResponse<E>) & {
     status: number;
     message: string;
 };
 
 export interface IHttpClient {
-    request<TData, TError = Error>(
+    request<TData = any, TError = any>(
+        method: HttpMethods,
+        url: string,
+        options?: HTTPClientOptions
+    ): Promise<HTTPClientResponse<TData, TError>>;
+}
+
+export abstract class AbstractHttpClient implements IHttpClient {
+    get<TData = any, TError = any>(url: string, options?: HTTPClientOptions): Promise<HTTPClientResponse<TData, TError>> {
+        return this.request("get", url, options);
+    }
+
+    post<TData = any, TError = any>(url: string, options?: HTTPClientOptions): Promise<HTTPClientResponse<TData, TError>> {
+        return this.request("post", url, options);
+    }
+
+    put<TData = any, TError = any>(url: string, options?: HTTPClientOptions): Promise<HTTPClientResponse<TData, TError>> {
+        return this.request("put", url, options);
+    }
+
+    patch<TData = any, TError = any>(url: string, options?: HTTPClientOptions): Promise<HTTPClientResponse<TData, TError>> {
+        return this.request("patch", url, options);
+    }
+
+    delete<TError = any>(url: string, options?: HTTPClientOptions): Promise<HTTPClientResponse<void, TError>> {
+        return this.request("delete", url, options);
+    }
+
+    abstract request<TData = any, TError = any>(
         method: HttpMethods,
         url: string,
         options?: HTTPClientOptions
