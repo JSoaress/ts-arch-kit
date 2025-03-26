b99e76ad61cf30444338129547bcaac23939f17d
João Vitor <joao_vitorsgs@hotmail.com>
2025-03-26 12:22:26 -0300

    feat(queue): retorna um boolean do metodo 'connect' na interface IQueue

--- src/queue/IQueue.ts
+++ src/queue/IQueue.ts
@@ -1,5 +1,5 @@
 export interface IQueue {
-    connect(): Promise<void>;
+    connect(): Promise<boolean>;
     close(): Promise<void>;
     on<TInput = unknown>(queueName: string, callback: (input: TInput) => Promise<void>): Promise<void>;
     publish(queueName: string, data: unknown): Promise<void>;
